c57c42baf7d7ca3dbfea5dadcc20185c4c258ba4
feaoki <feaoki@gmail.com>
2020-09-14 16:55:09 -0300

    incluido linha nova teste git clone

--- exemplo01.java
+++ exemplo01.java
@@ -8,7 +8,7 @@ public class exemplo01 {
 
         System.out.println("alteardo");
         System.out.println("alteardo versao");
-        System.out.println("alteardo versao nova branch dev");
+        System.out.println("alteardo linha 11");
         System.out.println("alteardo versao novo push");
         
 
